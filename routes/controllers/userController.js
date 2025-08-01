284f4f9d10c503259f90be4eb7274e86c3d5a6cb
nawahtkui <alnwyra@gmail.com>
2025-08-01 03:25:29 +0300

    Create userController.js

---
+++ routes/controllers/userController.js
@@ -0,0 +1,12 @@
+const userService = require('../services/userService');
+
+exports.getAllUsers = (req, res) => {
+  const users = userService.getAllUsers();
+  res.json(users);
+};
+
+exports.registerUser = (req, res) => {
+  const userData = req.body;
+  const newUser = userService.registerUser(userData);
+  res.status(201).json(newUser);
+};
