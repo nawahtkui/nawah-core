054bdb2cb498c16439a9d92c2796e2b730a8377c
nawahtkui <alnwyra@gmail.com>
2025-10-22 17:05:55 +0300

    Create config.ts
    Signed-off-by: nawahtkui <alnwyra@gmail.com>

---
+++ config.ts
@@ -0,0 +1,7 @@
+export const CONTRACT_ADDRESSES = {
+  Storage: "0x123...abc",   // عنوان عقد التخزين
+  Betting: "0x234...bcd",   // عقد المراهنة
+  Staking: "0x345...cde",   // عقد التكديس
+  Options: "0x456...def",   // عقد الخيارات
+  Futures: "0x567...ef0",   // عقد العقود الآجلة
+};
