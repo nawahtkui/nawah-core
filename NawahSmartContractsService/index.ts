fb44b9976a9baa36775ce9f3376a16b03660f3d1
nawahtkui <alnwyra@gmail.com>
2025-10-22 17:15:50 +0300

    Create index.ts
    Signed-off-by: nawahtkui <alnwyra@gmail.com>

---
+++ NawahSmartContractsService/index.ts
@@ -0,0 +1,37 @@
+import { ethers } from "ethers";
+import StorageABI from "./contracts/Storage.json";
+import BettingABI from "./contracts/Betting.json";
+import StakingABI from "./contracts/Staking.json";
+import OptionsABI from "./contracts/Options.json";
+import FuturesABI from "./contracts/Futures.json";
+import { CONTRACT_ADDRESSES } from "./config";
+
+export class NawahSmartContractsService {
+  provider: ethers.providers.Web3Provider | null = null;
+  signer: ethers.Signer | null = null;
+  contracts: Record<string, ethers.Contract> = {};
+
+  async init() {
+    if (!window.ethereum) throw new Error("MetaMask أو Web3 غير متوفر");
+    this.provider = new ethers.providers.Web3Provider(window.ethereum);
+    await this.provider.send("eth_requestAccounts", []);
+    this.signer = this.provider.getSigner();
+
+    this.contracts.storage = new ethers.Contract(CONTRACT_ADDRESSES.Storage, StorageABI, this.signer);
+    this.contracts.betting = new ethers.Contract(CONTRACT_ADDRESSES.Betting, BettingABI, this.signer);
+    this.contracts.staking = new ethers.Contract(CONTRACT_ADDRESSES.Staking, StakingABI, this.signer);
+    this.contracts.options = new ethers.Contract(CONTRACT_ADDRESSES.Options, OptionsABI, this.signer);
+    this.contracts.futures = new ethers.Contract(CONTRACT_ADDRESSES.Futures, FuturesABI, this.signer);
+  }
+
+  async callContract(contractName: string, method: string, params: any[] = []) {
+    if (!this.contracts[contractName]) throw new Error(`العقد ${contractName} غير موجود`);
+    return await this.contracts[contractName][method](...params);
+  }
+
+  async sendTransaction(contractName: string, method: string, params: any[] = []) {
+    if (!this.contracts[contractName]) throw new Error(`العقد ${contractName} غير موجود`);
+    const tx = await this.contracts[contractName][method](...params);
+    return await tx.wait();
+  }
+}
